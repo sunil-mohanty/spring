c17fe9c269174c580bbe2d01df46f5e7b2d4d952
sunil-mohanty <sunil.mohanty@live.com>
2019-06-25 01:49:50 +0530

    Demonstration mvc with SpringBoot

---
+++ SpringBootMVC/src/main/java/com/ski/spring/boot/Application.java
@@ -0,0 +1,30 @@
+package com.ski.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * 
+ * @author sunilmohanty
+ *
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    public ViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/view/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
+}
---
+++ SpringBootMVC/src/main/java/com/ski/spring/boot/LoginController.java
@@ -0,0 +1,42 @@
+package com.ski.spring.boot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ski.bean.LoginBean;
+
+/**
+ * 
+ * @author sunilmohanty
+ *
+ */
+@Controller
+public class LoginController {
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String init(Model model) {
+		model.addAttribute("msg", "Please Enter Your Login Details");
+		return "login";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String submit(Model model,
+			@ModelAttribute("loginBean") LoginBean loginBean) {
+		if (loginBean != null && loginBean.getUserName() != null
+				& loginBean.getPassword() != null) {
+			if (loginBean.getUserName().equals("krishna")
+					&& loginBean.getPassword().equals("krishna")) {
+				model.addAttribute("msg", loginBean.getUserName());
+				return "success";
+			} else {
+				model.addAttribute("error", "Invalid Details");
+				return "login";
+			}
+		} else {
+			model.addAttribute("error", "Please enter Details");
+			return "login";
+		}
+	}
+}
